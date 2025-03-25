fd06a996cf77a8373354589b0c3140498f31adae
jfoote22 <justinfoote@gmail.com>
2025-03-25 11:15:38 -0500

    refactor: rename 'My Images' tab to 'My Models'

--- src/app/components/ImageGenerator.tsx
+++ src/app/components/ImageGenerator.tsx
@@ -652,7 +652,7 @@ export default function ImageGenerator() {
                       </span>
                     </a>
                     
-                    {/* Save to My Images button - now positioned below 3D model */}
+                    {/* Save to My Models button - now positioned below 3D model */}
                     {modelUrl && !generating3D && (
                       <div className="mt-4">
                         <button
@@ -673,7 +673,7 @@ export default function ImageGenerator() {
                               <svg className="w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                                 <path d="M7.707 10.293a1 1 0 10-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 11.586V6h5a2 2 0 012 2v7a2 2 0 01-2 2H4a2 2 0 01-2-2V8a2 2 0 012-2h5v5.586l-1.293-1.293zM9 4a1 1 0 012 0v2H9V4z" />
                               </svg>
-                              Save to My Images
+                              Save to My Models
                             </span>
                           )}
                         </button>
@@ -684,7 +684,7 @@ export default function ImageGenerator() {
                               <svg className="w-4 h-4 mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                                 <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
                               </svg>
-                              Image and 3D model saved successfully
+                              Image and 3D model saved to My Models
                             </p>
                           </div>
                         )}
